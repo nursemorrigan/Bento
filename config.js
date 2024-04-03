bc5d47ad92aba0ec14e98fa5a72b816e8a5ed294
nursemorrigan <46274809+nursemorrigan@users.noreply.github.com>
2024-04-03 01:58:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -53,9 +53,9 @@ const CONFIG = {
 	firstButtonsContainer: [
 		{
 			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
+			name: 'Google',
+			icon: 'search',
+			link: 'https://google.ca',
 		},
 		{
 			id: '2',
